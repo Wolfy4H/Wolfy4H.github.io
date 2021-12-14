6f43d57df5ca17f09743d589dcd5e7b4586c7d24
Wolfy4H <Leon.Kersten@outlook.com>
2021-12-14 13:01:37 +0100

    Opzet Project

---
+++ js/typed.js
@@ -0,0 +1,17 @@
+const typed = new Typed(".auto-input", {
+  strings: [
+    "Leon Kersten",
+    "from the Netherlands",
+    "a software developer",
+    "good at making pixel art",
+    "a team player",
+    "good at helping people",
+    "a good listener",
+    "a gamer",
+    "a geometry dash player B)",
+  ],
+  typeSpeed: 85,
+  backSpeed: 90,
+  wait: 2,
+  loop: true,
+});
